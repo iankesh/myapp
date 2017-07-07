d7ca0fb27c134be47553588be29eace1a405d45d
iankesh <iankesh@hotmail.com>
2017-07-07 20:46:17 +0000

    Please work 3983333

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ resources :users
 root :"users#index"
 #root :to => 'home#index'
 
-  devise_for :users,
-    :skip       => [:registrations],
-    :path       => '/user',
-    :path_names => {
-      sign_in:  '/login',
-      sign_out: '/logout'
-    } do
-  end
+#  devise_for :users,
+ #   :skip       => [:registrations],
+  #  :path       => '/user',
+   # :path_names => {
+    #  sign_in:  '/login',
+     # sign_out: '/logout'
+   # } do
+ # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
